88e50de81ea78f254060c458b00865e9d63edd64
terence <terenceshawn18@outlook.com>
2023-12-12 19:10:55 +0300

    done

--- lib/count_sentences.py
+++ lib/count_sentences.py
@@ -1,4 +1,31 @@
 #!/usr/bin/env python3
 
+import re
 class MyString:
-  pass
+    def __init__(self, value=""):
+        self.value = value
+
+    @property
+    def value(self):
+        return self._value
+
+    @value.setter
+    def value(self, new_value):
+        if isinstance(new_value, str):
+            self._value = new_value
+        else:
+            print("The value must be a string.")
+
+    def is_sentence(self):
+        return self._value.endswith('.')
+
+    def is_question(self):
+        return self._value.endswith('?')
+
+    def is_exclamation(self):
+        return self._value.endswith('!')
+
+    def count_sentences(self):
+        # Split the value based on ".", "?", and "!" and count the non-empty parts
+        sentences = [sentence for sentence in re.split(r'[.!?]', self._value) if sentence]
+        return len(sentences)
